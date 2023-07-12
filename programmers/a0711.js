5b4739ca84ea016c05053f108524a338de7819ab
rkdbs <w2207@e-mirim.hs.kr>
2023-07-12 23:45:20 +0900

    서울에서 김서방 찾기

---
+++ programmers/a0711.js
@@ -0,0 +1,11 @@
+// 서울에서 김서방 찾기
+function solution(seoul) {
+    let x = 0;
+    for (let i = 0; i < seoul.length; i++) {
+      if (seoul[i] === "Kim") {
+        x = i;
+        break;
+      }
+    }
+    return "김서방은 " + x + "에 있다";
+  }
